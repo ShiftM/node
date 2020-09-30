147ee74be74114e0b9abc9a2d9c429e4f1d228c1
ShiftM <57879243+ShiftM@users.noreply.github.com>
2020-09-30 20:13:05 +0800

    variable port

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the home page');
-});
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the home page');
+// });
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
