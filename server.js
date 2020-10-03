f13ba3e50cdc7039d48307b17ba6df32c763deba
ShiftM <57879243+ShiftM@users.noreply.github.com>
2020-10-03 20:47:28 +0800

    SQL

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { time } = require('console');
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -95,7 +96,37 @@ app.post('/insert', (req, res) => {
         console.log("The password is: "+ obj.password);
         // var conn = con.getConnection();
 
-        connection.query('INSERT INTO customer (email, pass) VALUES (?,?)',[obj.email,obj.password], function(error, results, fields){
+        let date_ob = new Date();
+
+        // current date
+        // adjust 0 before single digit date
+        let date = ("0" + date_ob.getDate()).slice(-2);
+
+        // current month
+        let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+        // current year
+        let year = date_ob.getFullYear();
+
+        // current hours
+        let hours = date_ob.getHours();
+
+        // current minutes
+        let minutes = date_ob.getMinutes();
+
+        // current seconds
+        let seconds = date_ob.getSeconds();
+
+        // prints date in YYYY-MM-DD format
+        console.log(year + "-" + month + "-" + date);
+
+        // prints date & time in YYYY-MM-DD HH:MM:SS format
+        console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+
+        // prints time in HH:MM format
+        console.log(hours + ":" + minutes);
+
+        connection.query('INSERT INTO customer (email, pass, date, time) VALUES (?,?)',[obj.email,obj.password, year + "-" + month + "-" + date, hours + ":" + minutes], function(error, results, fields){
         if(error) throw error;
         console.log("Success!");
     });
